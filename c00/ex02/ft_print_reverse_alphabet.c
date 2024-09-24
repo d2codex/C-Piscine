a87af7ea4acc0848607266a8250dbf69c74218fc
d2 <d2.de.sousa@gmail.com>
2024-09-24 16:00:17 +0200

    add files

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diade-so <diade-so@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/28 20:20:55 by diade-so          #+#    #+#             */
+/*   Updated: 2024/08/29 18:52:15 by diade-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write (1, &letter, 1);
+		letter--;
+	}
+	write (1, "\n", 1);
+}
+
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+}
