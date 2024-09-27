b3dfca8e075139424e2af3cddeeda1c31d0b9efb
d2 <d2.de.sousa@gmail.com>
2024-09-27 10:24:30 +0200

    add files

---
+++ c00/ex07/ft_putnbr.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diade-so <diade-so@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 16:17:35 by diade-so          #+#    #+#             */
+/*   Updated: 2024/09/27 10:22:23 by diade-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+		write (1, "-2147483648", 11);
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+		ft_putnbr(nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + 48);
+	}
+	else
+		ft_putchar(nb + 48);
+}
+
+int	main(void)
+{
+	ft_putnbr(0);
+	ft_putchar('\n');
+	ft_putnbr(-2147483648);
+	ft_putchar('\n');
+	ft_putnbr(2147483647);
+	ft_putchar('\n');
+	ft_putnbr(10052);
+	ft_putchar('\n');
+	ft_putnbr(-854);
+	ft_putchar('\n');
+}
